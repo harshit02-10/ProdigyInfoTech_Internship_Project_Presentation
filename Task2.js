15e6b72092cc4fa49e576cf3ccfac1eb4b093539
Harshit Upadhyay <upadhyayh099@gmail.com>
2026-02-24 10:36:40 +0530

    Add files via upload

---
+++ Task2.js
@@ -0,0 +1,48 @@
+let timer;
+let seconds = 0;
+let minutes = 0;
+let hours = 0;
+let running = false;
+
+function updateDisplay() {
+    let h = hours < 10 ? "0" + hours : hours;
+    let m = minutes < 10 ? "0" + minutes : minutes;
+    let s = seconds < 10 ? "0" + seconds : seconds;
+
+    document.getElementById("display").innerText = h + ":" + m + ":" + s;
+}
+
+function start() {
+    if (!running) {
+        running = true;
+        timer = setInterval(function () {
+            seconds++;
+
+            if (seconds == 60) {
+                seconds = 0;
+                minutes++;
+            }
+
+            if (minutes == 60) {
+                minutes = 0;
+                hours++;
+            }
+
+            updateDisplay();
+        }, 1000);
+    }
+}
+
+function pause() {
+    clearInterval(timer);
+    running = false;
+}
+
+function reset() {
+    clearInterval(timer);
+    running = false;
+    seconds = 0;
+    minutes = 0;
+    hours = 0;
+    updateDisplay();
+}
